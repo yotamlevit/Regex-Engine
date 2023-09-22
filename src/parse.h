7364c8f7d467c21d74bc970f7f03c6e0a807cbbe
yotamlevit <yotam.levit@gmail.com>
2023-09-22 18:11:39 +0300

    Added parse header file

---
+++ src/parse.h
@@ -0,0 +1,10 @@
+//
+// Created by Yotam Levit on 22/09/2023.
+//
+#pragma once
+#ifndef REGEX_ENGINE_PARSE_H
+#define REGEX_ENGINE_PARSE_H
+
+char* infixToPostfix(char * infix);
+
+#endif //REGEX_ENGINE_PARSE_H
