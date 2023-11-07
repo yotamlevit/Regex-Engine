66f8308278a3760b8e5036c3988ea9cb58430cb3
yotamlevit <yotam.levit@gmail.com>
2023-11-07 08:45:20 +0200

    Change state statue numbers

--- Include/nfa.h
+++ Include/nfa.h
@@ -10,9 +10,10 @@
 typedef enum { FALSE, TRUE } BOOLEAN;
 #endif
 
+
 typedef struct State
 {
-    char c; // Represents the char | 3 in ascii represents a true state | 2 represents epsilon
+    char c; // Represents the char c<256 | c=256 in ascii represents a split | c=256 represents a match
     struct State *out, *out1; // Pointer to one of the next states | Pointer to the second of the next states
     int lastlist; // Lastlist is used during execution and is explained in the next section.
 }NFA,* NfaPtr;
@@ -20,7 +21,7 @@ typedef struct State
 
 // Function declaretion
 NfaPtr initNFA();
-NfaPtr initNFAData(char c);
+NfaPtr initNFAData(char c, NfaPtr out, NfaPtr out1, int lastlist);
 void setNext(NfaPtr pNfa, char c);
 BOOLEAN isEndState(NfaPtr pNFA);
 
