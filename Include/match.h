0254ec41b5d7e00791d52543ada550749a117258
yotamlevit <yotam.levit@gmail.com>
2024-02-18 10:08:53 +0200

    Added header file to match

--- Include/match.h
+++ Include/match.h
@@ -13,26 +13,6 @@
 #include "Include/nfa.h"
 #include <stdlib.h>
 
-typedef struct List
-{
-    NfaPtr *s;
-    int n;
-}List;
-
-List list1, list2;
-int listId;
-#define SPLIT 257
-#define MATCH 256
-
-int ismatch(List *l);
-
-void addstate(List *l, NfaPtr s);
-
-List* startlist(NfaPtr s, List *l);
-
-void step(List *currentList, int c, List *nextList);
-
-
 int match(NfaPtr start, char *s);
 
 #endif //REGEX_ENGINE_MATCH_H
